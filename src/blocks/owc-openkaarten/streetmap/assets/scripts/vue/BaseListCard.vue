12c5a0234a8a2fc07e522f3f972d812b7f740f66
Mark Teekman <128050096+markacato@users.noreply.github.com>
2024-10-24 17:08:18 +0200

    Card and button styling on the list view page. OWC-95

---
+++ src/blocks/owc-openkaarten/streetmap/assets/scripts/vue/BaseListCard.vue
@@ -0,0 +1,100 @@
+<script setup>
+defineProps({
+	title: {
+		type: String,
+		required: true,
+	},
+	address: {
+		type: String,
+		default: '',
+	},
+	description: {
+		type: String,
+		default: '',
+	},
+	image: {
+		type: String,
+		default: '',
+	},
+	primaryColor: {
+		type: String,
+		default: '#328725',
+	},
+});
+</script>
+
+<template>
+	<div class="base-list-card">
+		<div class="base-list-card__content">
+			<h3 class="base-list-card__title">{{ title }}</h3>
+			<p class="base-list-card__address" v-if="address">{{ address }}</p>
+			<p class="base-list-card__description" v-if="description">{{ description }}</p>
+			<a href="#" class="base-list-card__link">
+				<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+					<path d="M10.243 4.41a.833.833 0 0 1 1.178 0l5 5a.833.833 0 0 1 0 1.18l-5 5a.833.833 0 0 1-1.178-1.18l3.577-3.577H4.165a.833.833 0 0 1 0-1.667h9.655L10.243 5.59a.833.833 0 0 1 0-1.178Z" fill="#1261A3"/>
+				</svg>
+				Lees meer
+			</a>
+		</div>
+		<div class="base-list-card__image" v-if="image">
+			<img :src="image" :alt="title" />
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.base-list-card {
+	--image-size: 256px;
+
+	display: flex;
+	gap: 1rem;
+	border: 1px solid #cacaca;
+	border-radius: 6px;
+
+	&__image {
+		flex: 0 0 var(--image-size);
+		aspect-ratio: 1 / 1;
+
+		img {
+			width: 100%;
+			height: 100%;
+			object-fit: cover;
+		}
+	}
+
+	&__content {
+		flex: 1;
+		padding: 2rem;
+	}
+
+	&__title {
+		margin: 0;
+		color: #001d5f;
+		font-size: 1.5rem;
+		font-weight: bold;
+	}
+
+	&__address {
+		font-size: 14px;
+		color: #4b4b4b
+	}
+
+	&__description {
+		color: #4b4b4b;
+		font-weight: 400;
+	}
+
+	&__link {
+		display: flex;
+		align-items: center;
+		gap: 0.25rem;
+		margin-block-start: 2rem;
+		color: #1161a3;
+		text-decoration: none;
+
+		&:hover {
+			text-decoration: underline;
+		}
+	}
+}
+</style>
--- src/blocks/owc-openkaarten/streetmap/assets/scripts/vue/ListView.vue
+++ src/blocks/owc-openkaarten/streetmap/assets/scripts/vue/ListView.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { computed, ref } from 'vue';
-import BaseTooltipCard from './BaseTooltipCard.vue';
+import BaseListCard from './BaseListCard.vue';
+import { makeFilterButtonHTML } from '../utils/make-filter-button-html';
+import { makeMapButtonHTML } from '../utils/make-map-button-html';
 
 const props = defineProps({
   datasets: {
@@ -27,11 +29,7 @@ const filteredLocations = computed(() => {
     })));
 });
 
-const emits = defineEmits(['toggleView', 'closeCard']);
-
-const closeCard = () => {
-  emits('closeCard');
-};
+const emits = defineEmits(['toggleView']);
 
 const showFilters = ref(false);
 
@@ -43,6 +41,9 @@ const toggleView = () => {
   emits('toggleView');
 };
 
+const filterButtonHTML = computed(() => makeFilterButtonHTML('Filter', props.primaryColor));
+const mapButtonHTML = computed(() => makeMapButtonHTML('Kaart', props.primaryColor));
+
 // @TODO: Implement the actual filtering logic here
 // ...
 </script>
@@ -50,85 +51,72 @@ const toggleView = () => {
 <template>
   <div class="list-view">
     <div class="list-view__controls">
-      <button @click="toggleView" class="list-view__map-button">
-        Kaart
-      </button>
-      <button @click="toggleFilters" class="list-view__filters-button">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M9 5C8.44772 5 8 5.44772 8 6C8 6.55228 8.44772 7 9 7C9.55228 7 10 6.55228 10 6C10 5.44772 9.55228 5 9 5ZM6.17071 5C6.58254 3.83481 7.69378 3 9 3C10.3062 3 11.4175 3.83481 11.8293 5H19C19.5523 5 20 5.44772 20 6C20 6.55228 19.5523 7 19 7H11.8293C11.4175 8.16519 10.3062 9 9 9C7.69378 9 6.58254 8.16519 6.17071 7H5C4.44772 7 4 6.55228 4 6C4 5.44772 4.44772 5 5 5H6.17071ZM15 11C14.4477 11 14 11.4477 14 12C14 12.5523 14.4477 13 15 13C15.5523 13 16 12.5523 16 12C16 11.4477 15.5523 11 15 11ZM12.1707 11C12.5825 9.83481 13.6938 9 15 9C16.3062 9 17.4175 9.83481 17.8293 11H19C19.5523 11 20 11.4477 20 12C20 12.5523 19.5523 13 19 13H17.8293C17.4175 14.1652 16.3062 15 15 15C13.6938 15 12.5825 14.1652 12.1707 13H5C4.44772 13 4 12.5523 4 12C4 11.4477 4.44772 11 5 11H12.1707ZM9 17C8.44772 17 8 17.4477 8 18C8 18.5523 8.44772 19 9 19C9.55228 19 10 18.5523 10 18C10 17.4477 9.55228 17 9 17ZM6.17071 17C6.58254 15.8348 7.69378 15 9 15C10.3062 15 11.4175 15.8348 11.8293 17H19C19.5523 17 20 17.4477 20 18C20 18.5523 19.5523 19 19 19H11.8293C11.4175 20.1652 10.3062 21 9 21C7.69378 21 6.58254 20.1652 6.17071 19H5C4.44772 19 4 18.5523 4 18C4 17.4477 4.44772 17 5 17H6.17071Z" fill="${color}"/>
-        </svg>
-        <span class="leaflet-control-filters__control-text">Filter</span>
-      </button>
+      <button @click="toggleView" class="list-view__map-button" v-html="mapButtonHTML"></button>
+      <button @click="toggleFilters" class="list-view__filters-button" v-html="filterButtonHTML"></button>
     </div>
     <div v-for="location in filteredLocations" :key="`${location.datasetId}-${location.properties.id}`" class="list-view__item">
-      <div class="list-view__item-content">
-        <h3>{{ location.properties.title || location.datasetTitle }}</h3>
-        <p>{{ location.properties.address }}</p>
-        <p>{{ location.properties.description }}</p>
-        <a href="#" class="list-view__item-link">Lees meer</a>
-      </div>
-      <div class="list-view__item-image" v-if="location.properties.image">
-        <img :src="location.properties.image" :alt="location.properties.title || location.datasetTitle">
-      </div>
+      <BaseListCard
+        :title="location.properties.title || location.datasetTitle"
+        :address="location.properties.address || 'Zorg | Beatrixlaan 32 C, 3273 AB Westmaas'"
+        :description="location.properties.description || 'Huisartsenpraktijk Westmaas B.V. ligt in het gelijknamige dorp in de regio Hoeksche Waard. Westmaas telt circa 2.065 inwoners en levert huisartsenzorg aan 2.380 patiënten.'"
+        :image="location.properties.image || 'https://images.unsplash.com/photo-1494526585095-c41746248156?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'"
+        :primaryColor="primaryColor"
+      >
+        <template #footer>
+        </template>
+      </BaseListCard>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .list-view {
-  padding: 20px;
-  overflow-y: auto;
-  height: 100%;
-}
-
-.list-view__item {
   display: flex;
-  margin-bottom: 20px;
-  border: 1px solid #e0e0e0;
-  border-radius: 4px;
-  overflow: hidden;
+  flex-direction: column;
+  gap: 1rem;
 
-  &-content {
-    flex: 1;
-    padding: 15px;
-  }
+  &__controls {
+    display: flex;
+    justify-content: flex-end;
+    gap: 1rem;
+    margin-block-end: 0.5rem;
 
-  &-image {
-    width: 200px;
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
+    button {
+      align-items: center;
+      border: 1px solid #328725;
+      background-color: #fff;
+      border-radius: 3px;
+      display: flex;
+      justify-content: space-between;
+      gap: 8px;
+      min-width: 44px;
+      min-height: 50px;
+      padding: 10px 24px;
+
+      // @TODO: styles are not being applied, why?
+      span {
+        color: #328725;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 130%; 
+      }
+
+      &:hover {
+        cursor: pointer;
+        background-color: rgb(244, 244, 244);
+      }
+
+      &:focus-visible {
+        border-width: 3px;
+      }
     }
   }
 
-  &-link {
-    color: #007bff;
-    text-decoration: none;
-    
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-}
-
-.list-view__controls {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  z-index: 1000;
-}
-
-.list-view__control-button {
-  background-color: white;
-  border: 2px solid rgba(0,0,0,0.2);
-  border-radius: 4px;
-  padding: 5px 10px;
-  cursor: pointer;
-  font-size: 14px;
-  
-  &:hover {
-    background-color: #f4f4f4;
+  &__item {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
   }
 }
 </style>
