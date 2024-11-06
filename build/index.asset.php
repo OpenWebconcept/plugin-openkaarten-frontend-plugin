0ab8dd9990cc3d7161605a5fdf9deac69801870f
Dik Takken <dik.takken@yard.nl>
2024-11-06 17:11:59 +0100

    Update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6e6a8552bf224450828a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '238d3706dda66628db65');
