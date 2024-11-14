6e549086bc30581cbb6584e544ff3146339e5881
Mark Teekman <128050096+markacato@users.noreply.github.com>
2024-11-14 10:26:56 +0100

    Remove dummy data. OWC-95

--- src/blocks/owc-openkaarten/streetmap/assets/scripts/utils/make-tooltip-card.js
+++ src/blocks/owc-openkaarten/streetmap/assets/scripts/utils/make-tooltip-card.js
@@ -8,9 +8,9 @@ export const makeTooltipCard = (location, dataset) => {
 		datasetType: type,
 		locationTitle: tooltipData.find(t => t.layout === 'title')?.title || location.title,
 		meta: tooltipData.find(t => t.layout === 'meta')?.meta || '',
-		text: tooltipData.find(t => t.layout === 'text')?.text || 'Huisartsenpraktijk Westmaas B.V. ligt in het gelijknamige dorp in de regio Hoeksche Waard. Westmaas telt circa 2.065 inwoners en levert huisartsenzorg aan 2.380 patiënten.',
+		text: tooltipData.find(t => t.layout === 'text')?.text || '',
 		button: tooltipData.find(t => t.layout === 'button') || null,
-		image: tooltipData.find(t => t.layout === 'image')?.image || 'https://images.unsplash.com/photo-1525916801717-9405b53a3246?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNwb3J0JTIwcGFya3xlbnwwfHwwfHx8MA%3D%3D',
+		image: tooltipData.find(t => t.layout === 'image')?.image || '',
 		properties: location.properties,
 		coordinates: location.geometry.coordinates
 	};
--- src/blocks/owc-openkaarten/streetmap/assets/scripts/vue/ListView.vue
+++ src/blocks/owc-openkaarten/streetmap/assets/scripts/vue/ListView.vue
@@ -31,9 +31,9 @@ const filteredLocations = computed(() => {
         datasetTitle: dataset.title,
         title: tooltipData.find(t => t.layout === 'title')?.title || feature.title,
         meta: tooltipData.find(t => t.layout === 'meta')?.meta || '',
-        text: tooltipData.find(t => t.layout === 'text')?.text || 'Huisartsenpraktijk Westmaas B.V. ligt in het gelijknamige dorp in de regio Hoeksche Waard. Westmaas telt circa 2.065 inwoners en levert huisartsenzorg aan 2.380 patiënten. ',
+        text: tooltipData.find(t => t.layout === 'text')?.text || '',
         button: tooltipData.find(t => t.layout === 'button') || null,
-        image: tooltipData.find(t => t.layout === 'image')?.image || 'https://images.unsplash.com/photo-1525916801717-9405b53a3246?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHNwb3J0JTIwcGFya3xlbnwwfHwwfHx8MA%3D%3D'
+        image: tooltipData.find(t => t.layout === 'image')?.image || ''
       };
     }));
 });
