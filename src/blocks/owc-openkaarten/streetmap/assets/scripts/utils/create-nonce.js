6b3334ef72e8972e1ed7a407e1f20defca4dd616
Dik Takken <dik.takken@yard.nl>
2025-02-12 11:40:45 +0100

    Allow passing a CSP nonce for WebPack style loader

---
+++ src/blocks/owc-openkaarten/streetmap/assets/scripts/utils/create-nonce.js
@@ -0,0 +1,5 @@
+// This allows passing a nonce for the style loader to
+// make it work in strict CSP environments
+if (window?.__webpack_nonce__) {
+    __webpack_nonce__ = window.__webpack_nonce__;
+}
