d34bb46b6ce1fd94064f048bea8b1fbebc68ad92
YvetteNikolov <yvettenikolov@gmail.com>
2026-02-24 16:50:52 +0100

    feat: improve global window api with a per-map api

--- src/blocks/owc-openkaarten/streetmap/assets/scripts/utils/map-window-api.js
+++ src/blocks/owc-openkaarten/streetmap/assets/scripts/utils/map-window-api.js
@@ -1,31 +1,117 @@
-import L from 'leaflet';
+import L from 'leaflet'; // eslint-disable-line import/no-unresolved
 
-if (!window.openkaarten) window.openkaarten = {};
+if (!window.openkaarten) {
+	window.openkaarten = {};
+}
 
-window.openkaarten.maps = window.openkaarten.maps || {};
-
-window.openkaarten.registerMap = function(mapId, mapInstance) {
-  if (!mapId || !mapInstance) return;
-  window.openkaarten.maps[mapId] = mapInstance;
+const store = {
+	maps: {},
+	apis: {},
 };
 
-window.openkaarten.addMarker = function(mapId, options = {}) {
-  const map = window.openkaarten.maps[mapId];
-  if (!map) return;
-  const {
-	lat,
-	lng,
-	popup,
-	markerOptions = {},
-	flyTo = true,
-	flyToOptions = {},
-	onAdd
-  } = options;
-  if (typeof lat !== 'number' || typeof lng !== 'number') return;
+/**
+ * Register map (called from Vue)
+ */
+window.openkaarten.registerMap = (mapId, mapInstance) => {
+	if (!mapId || !mapInstance) return;
 
-  const marker = L.marker([lat, lng], markerOptions).addTo(map);
-
-  if (popup) marker.bindPopup(popup);
-  if (flyTo) map.flyTo([lat, lng], flyToOptions.zoom || 15, { animate: true, duration: 1, ...flyToOptions });
-  if (typeof onAdd === 'function') onAdd(marker, map);
+	store.maps[mapId] = mapInstance;
+	store.apis[mapId] = createMapAPI(mapId, mapInstance);
 };
+
+/**
+ * Get map API
+ */
+window.openkaarten.getMap = (mapId) => {
+	return store.apis[mapId] || null;
+};
+
+/**
+ * Create per-map API
+ */
+const createMapAPI = (mapId, map) => {
+	const layers = new Map();
+
+	const ensureLayer = (name = 'default') => {
+		if (!layers.has(name)) {
+			const layerGroup = L.layerGroup().addTo(map);
+			layers.set(name, layerGroup);
+		}
+		return layers.get(name);
+	};
+
+	const addMarker = ({
+		lat,
+		lng,
+		popup,
+		markerOptions = {},
+		flyTo = false,
+		flyToOptions = {},
+		layer = 'default',
+		onAdd,
+	} = {}) => {
+		if (typeof lat !== 'number' || typeof lng !== 'number') {
+			return null;
+		}
+
+		const targetLayer = ensureLayer(layer);
+
+		const marker = L.marker([lat, lng], markerOptions);
+		targetLayer.addLayer(marker);
+
+		if (popup) {
+			marker.bindPopup(popup);
+		}
+
+		if (flyTo) {
+			const {
+				zoom = map.getZoom(),
+				duration = 1,
+				...rest
+			} = flyToOptions;
+
+			map.flyTo([lat, lng], zoom, {
+				animate: true,
+				duration,
+				...rest,
+			});
+		}
+
+		if (typeof onAdd === 'function') {
+			onAdd(marker, map);
+		}
+
+		return marker;
+	};
+
+	const removeLayer = (name) => {
+		const layer = layers.get(name);
+		if (!layer) return;
+
+		map.removeLayer(layer);
+		layers.delete(name);
+	};
+
+	const clearLayer = (name = 'default') => {
+		const layer = layers.get(name);
+		if (!layer) return;
+
+		layer.clearLayers();
+	};
+
+	const destroy = () => {
+		layers.forEach(layer => {
+			map.removeLayer(layer);
+		});
+		layers.clear();
+	};
+
+	return {
+		map,
+		addMarker,
+		removeLayer,
+		clearLayer,
+		destroy,
+	};
+};
+
