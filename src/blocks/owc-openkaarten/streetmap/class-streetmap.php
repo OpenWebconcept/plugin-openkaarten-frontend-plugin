5b1b1d838a3cee8fd9a035fc9eb2e6d9c42b9ec1
Eyal Beker <eyal@acato.nl>
2026-04-29 11:47:40 +0200

    Fix for retrieving icons from the right filepath

--- src/blocks/owc-openkaarten/streetmap/class-streetmap.php
+++ src/blocks/owc-openkaarten/streetmap/class-streetmap.php
@@ -26,6 +26,14 @@ use Openkaarten_Frontend_Plugin\Includes\Base_Block;
  */
 class Streetmap extends Base_Block {
 	/**
+	 * Streetmap constructor.
+	 */
+	public function __construct() {
+		parent::__construct();
+		add_action( 'enqueue_block_assets', [ $this, 'localize_script' ], 11 );
+	}
+
+	/**
 	 * Render the blocks HTML.
 	 *
 	 * @param array  $attributes An array of block attributes.
@@ -39,6 +47,20 @@ class Streetmap extends Base_Block {
 		$output = ob_get_clean();
 
 		return $output;
+	}
+
+	/**
+	 * Localize the streetmap view script with the icon base URL derived from
+	 * the openkaarten-base plugin's location.
+	 */
+	public function localize_script() {
+		wp_localize_script(
+			'owc-openkaarten-streetmap-block',
+			'openkaartenStreetmap',
+			[
+				'iconBaseUrl' => plugins_url( 'plugin-openkaarten-base/opengemeenten-iconenset/Regular/' ),
+			]
+		);
 	}
 }
 
