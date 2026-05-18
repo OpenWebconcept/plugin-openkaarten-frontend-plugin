c6f4fb3448ece04aeb2e077cf431025c26842317
Merel Voorn - van Uffelen <merel@acato.nl>
2026-05-18 13:58:32 +0200

    AS-22435

--- src/blocks/owc-openkaarten/streetmap/assets/scripts/vue/BaseListCard.vue
+++ src/blocks/owc-openkaarten/streetmap/assets/scripts/vue/BaseListCard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { computed } from 'vue';
-import BaseIcon from "./BaseIcon.vue";
+import TooltipContent from './TooltipContent.vue';
 
 const props = defineProps({
 	tooltipArray: {
@@ -35,37 +35,14 @@ const images = computed(
 	<div class="base-list-card">
 		<div class="base-list-card__inner">
 			<div class="base-list-card__content">
-				<template v-for="(item, index) in tooltipArray" :key="index">
-					<h3
-						v-if="item.layout === 'title' && item.title"
-						class="base-list-card__title"
-					>
-						{{ item.title }}
-					</h3>
-					<div
-						v-else-if="item.layout === 'meta' && item.meta"
-						class="base-list-card__meta"
-					>
-						<BaseIcon :marker="marker" />
-						<p class="base-list-card__address">{{ item.meta }}</p>
-					</div>
-					<p
-						v-else-if="item.layout === 'text' && item.text"
-						class="base-list-card__description"
-						v-html="item.text"
-					/>
-					<a
-						v-else-if="item.layout === 'button' && item.button_url"
-						:href="item.button_url"
-						class="base-list-card__button"
-					>
-						<svg aria-hidden="true" width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M10.243 4.41a.833.833 0 0 1 1.178 0l5 5a.833.833 0 0 1 0 1.18l-5 5a.833.833 0 0 1-1.178-1.18l3.577-3.577H4.165a.833.833 0 0 1 0-1.667h9.655L10.243 5.59a.833.833 0 0 1 0-1.178Z" fill="#fff"/>
-						</svg>
-						{{ item.button_text }}
-						<span class="sr-only">over {{ title }}</span>
-					</a>
-				</template>
+				<TooltipContent
+					:tooltipArray="tooltipArray"
+					classPrefix="base-list-card"
+					:title="title"
+					titleTag="h3"
+					:marker="marker"
+					:skipLayouts="['image']"
+				/>
 			</div>
 		</div>
 		<div class="base-list-card__image" v-if="images.length">
@@ -73,7 +50,7 @@ const images = computed(
 				v-for="(img, idx) in images"
 				:key="idx"
 				:src="img.image_url"
-				:alt="img.image_alt || title"
+				:alt="img.image_alt || ''"
 			/>
 		</div>
 	</div>
@@ -83,83 +60,88 @@ const images = computed(
 .base-list-card {
 	--image-size: 256px;
 
-  background-color: #fff;
-  border: 1px solid #cacaca;
-  border-radius: 6px;
+	background-color: #fff;
+	border: 1px solid #cacaca;
+	border-radius: 6px;
 	display: flex;
-  flex-direction: column-reverse;
+	flex-direction: column-reverse;
 	gap: 1rem;
-  margin-block-end: 1rem;
-  @media only screen and (min-width: 768px) {
-    flex-direction: row;
-  }
-  .filters-open & {
-    margin-inline-end: 1rem;
-  }
+	margin-block-end: 1rem;
+
+	@media only screen and (min-width: 768px) {
+		flex-direction: row;
+	}
+
+	.filters-open & {
+		margin-inline-end: 1rem;
+	}
 
 	&__image {
-    display: flex;
+		display: flex;
 		flex: 0 0 var(--image-size);
 		flex-direction: column;
 		gap: .5rem;
 
 		img {
-      aspect-ratio: 1 / 1;
-      inline-size: 100%;
-			object-fit: cover;
+			aspect-ratio: 1;
+			inline-size: 100%;
+			object-fit: none;
 		}
 	}
 
-  &__inner {
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    justify-content: space-between;
-    padding: 1.5rem;
-    padding-inline-end: .5rem;
-  }
+	&__inner {
+		display: flex;
+		flex: 1;
+		flex-direction: column;
+		justify-content: space-between;
+		padding: 1.5rem;
+		padding-inline-end: .5rem;
+	}
 
 	&__content {
 		display: flex;
 		flex-direction: column;
-    gap: .5rem;
-    margin-block-end: .5rem;
+		gap: .5rem;
+		margin-block-end: .5rem;
 	}
 
 	&__title {
 		color: #001d5f;
 		font-size: 1.5rem;
 		font-weight: bold;
-    margin: 0;
+		margin: 0;
 	}
-  &__meta {
-    align-items: center;
-    display: flex;
-    gap: .5rem;
-  }
+
+	&__meta {
+		align-items: center;
+		display: flex;
+		gap: .5rem;
+	}
+
 	&__address {
-    color: #4b4b4b;
+		color: #4b4b4b;
 		font-size: 14px;
 	}
 
-	&__description {
+	&__text {
 		color: #4b4b4b;
 		font-weight: 400;
 	}
 
-  &__button {
-    align-items: center;
-    background-color: var(--owc-openkaarten-streetmap--primary-color);
-    border-radius: 4px;
-    color: white;
-    display: inline-flex;
-    gap: 8px;
-    inline-size: fit-content;
-    padding: 8px 16px;
-    text-decoration: none;
-    &:hover {
-      opacity: 0.9;
-    }
-  }
+	&__button {
+		align-items: center;
+		background-color: var(--owc-openkaarten-streetmap--primary-color);
+		border-radius: 4px;
+		color: white;
+		display: inline-flex;
+		gap: 8px;
+		inline-size: fit-content;
+		padding: 8px 16px;
+		text-decoration: none;
+
+		&:hover {
+			opacity: 0.9;
+		}
+	}
 }
 </style>
